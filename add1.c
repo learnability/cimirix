af8d6837417c22d1d20bd52b8829416f5431bd6a
prabhavad <prabhavad@gmail.com>
2014-02-15 16:00:45 +0530

    Calc saved to add1.c

---
+++ add1.c
@@ -0,0 +1,120 @@
+#include<stdio.h>
+#include<math.h>
+
+int add()
+//Function to add two numbers
+{
+	printf("Enter first number: ";
+	int a, b, sum;
+	scanf("%d", &a);
+	
+	printf("Enter second number: ";
+	scanf("%d", &b);
+	printf("The sum of the numbers is: %d", sum=a+b);
+	return 1;
+}
+
+int subtract()					
+//FUNCTION FOR SUBTRACTING TWO NUMBERS		
+{
+	int a,b,s;                           //initializing
+	printf("Enter the two numbers: ";	
+	scanf("%d %d", &a,&b);		     //input from user
+	s=a-b;							//aDDING a and b
+	printf("%d-%d=%d",a,b,s);				//printing result
+	return 0;
+}
+	
+void mlpy() 																				/*Function for multiplication*/
+{
+	int a,b;
+	printf(" Enter two numbers:"); 
+	scanf ("%d %d", &a,&b); 															/*Asking the user for two inputs*/
+	printf(" %d x %d = %d\n",a ,b, a*b);												/*Printing*/
+}
+
+void div(float m, float n)
+{
+  float a,b,ans;
+  printf("Enter ur first number\n");
+  scanf("%f",&a);
+  printf("Enter ur second number \n");
+  scanf("%f",&b);
+  if(n!=0)
+  ans= m/n;
+   printf("Quotent : %f", ans);
+ }
+ 
+int mod()
+{
+	printf("Enter 2 numbers: ");
+	int a,b,m;
+	scanf("%d %d", a,b);
+	if(a>=b)
+	{
+		m=a%b;
+		printf("%d % %d = %d",a,b,m);
+	}
+	else if(b>a)
+	{
+		printf("Error! 1st number has to be greater than 2nd number");
+	}
+	return 0;
+}
+	
+
+ 
+ void ln()
+{	
+	float a,b;
+	printf("Enter a number");
+	scanf("%f", &a);
+	b=log(a);
+	printf("log(%f)=%f",a,b);
+} 
+ 	
+int main()
+{
+	printf("Welcome to a Simple Calculator");
+	
+	printf("\n\nChoose an operation:\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Log\n6.Power\n7.Mod\n8.Exit");
+	int choice;
+	scanf("%d", &choice);
+	switch(choice)
+	{
+		case 1:
+			add();
+			main();
+			
+		case 2:
+			subtract();
+			main();
+		case 3:
+			mltpy();
+			main();
+			
+		case 4:
+			div();
+			main();
+		case 5:
+			ln();
+			main();
+		case 6:
+			pow1();
+			main();
+		case 7:
+			mod();
+			main();
+		case 8:
+			exit(0);
+		default:
+			printf("\nInvalid Choice ! Try again !! "); 
+			
+	}	
+	
+	return 0;
+}
+
+	
+	
+	
