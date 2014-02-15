0fc0a586077cec642045adaab2c58f63218b8404
ritvik <ritvikvinod@gmail.com>
2014-02-15 12:39:52 +0530

    adding substract

---
+++ substract.c
@@ -0,0 +1,9 @@
+int subtract()							//FUNCTION FOR SUBTRACTING TWO NUMBERS		
+{
+	int a,b,s;							//initializing
+	printf("Enter the two numbers: ";
+	scanf("%d %d", &a,&b);					//input from user
+	s=a-b;							//aDDING a and b
+	printf("%d-%d=%d",a,b,s);				//printing result
+	return 0;
+}
