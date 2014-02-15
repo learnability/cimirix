e7a58baae653045bc826b3c6c221da977a126556
akkyrokz <guest@nsl58.(none)>
2014-02-15 14:17:23 +0530

    pow added

---
+++ pow1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+include<math.h>
+int main()
+{
+float=a,b,ans;
+printf("Enter you number and its power\n");
+scanf("%f,%f",&a,&b);
+ans=pow(a,b);
+printf("%f",ans);
+return o;
+}
