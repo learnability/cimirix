06b09f099da0571ad6fb33b21ac74e2b2d59af41
Guest <guest@nsl56.(none)>
2014-02-15 12:32:21 +0530

    multiply added

---
+++ multiply.c
@@ -0,0 +1,7 @@
+int mlpy() 																				/*Function for multiplication*/
+{
+	int a,b;
+	printf(" Enter two numbers:"); 
+	scanf ("%d %d", &a,&b); 															/*Asking the user for two inputs*/
+	printf(" %d x %d = %d\n",a ,b, a*b);												/*Prin
+}
